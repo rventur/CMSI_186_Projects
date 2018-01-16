20799b504c6b992586964a3b04bce5977998eaef
rventur <35280883+rventur@users.noreply.github.com>
2018-01-15 23:13:42 -0800

    Create Homeworkzero.java
    First Homework Assignment

---
+++ Homework00/Homeworkzero.java
@@ -0,0 +1,14 @@
+/*purpose: To become familiar with writing 
+ compiling, and running java programs
+ Due Date: Jan 16,2018
+ @author:Roberto Ventura
+ @version 1.0 */
+
+public class Homework {
+
+    public static void main(String[] args) {
+	
+	System.out.println("Hello World");
+	
+	}
+}
