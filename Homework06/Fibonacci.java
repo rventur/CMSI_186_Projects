bc099e27814da4e1fb7709e2c7a71f7fe88d4c41
rventur <35280883+rventur@users.noreply.github.com>
2018-04-21 04:16:31 -0700

    write method for fibonacci sequence

--- Homework06/Fibonacci.java
+++ Homework06/Fibonacci.java
@@ -1,9 +1,8 @@
 /** ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
  * File name  :  Fibonacci.java
  * Purpose    :  Find the "nth" Fibonacci number given an argument, using BrobInt class
- * @author    :  B.J. Johnson
  * @author    :  Bob
- * Date       :  2017-04-17
+ * Date       :  2018-04-09
  * Description:  @see <a href='http://bjohnson.lmu.build/cmsi186web/homework06.html'>Assignment Page</a>
  * Notes      :  None
  * Warnings   :  None
@@ -14,6 +13,7 @@
  *   Ver      Date     Modified by:  Reason for change or modification
  *  -----  ----------  ------------  ---------------------------------------------------------------------
  *  1.0.0  2017-04-17  B.J. Johnson  Initial writing and begin coding
+ *  1.0.1  2018-04-17  Bob           Finish coding
  *
  *  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ */
 
@@ -84,8 +84,24 @@ public class Fibonacci {
          System.out.println( "\n                This may take me a while; please be patient!!\n\n" );
       }
 
-  //    System.out.println( "\n\n\n  ...HA!! Like I'm going to do the ENTIRE thing for you.....  *grins*" );
-
+	  if (1 == maxCount) {
+		  System.out.println("First term is: " + 0 );
+	  }
+	  else if (2 == maxCount) {
+		  System.out.println("Second Term is: " + 1);
+	  }
+	  else if (3 == maxCount) {
+		  System.out.println("Third Term is: " + 1);
+	  }
+	  BrobInt previous1 = new BrobInt("0");
+	  BrobInt previous2 = new BrobInt("1");
+	  BrobInt fibonnaci = null;
+	  for (int i = 2; i < maxCount; i++) {
+		  fibonnaci = previous1.add(previous2);
+		  previous1 = previous2;
+		  previous2 = fibonnaci;
+	  }
+	  System.out.println("The nth term is: " + String.valueOf(fibonnaci));
 
       System.exit( 0 );
    }
